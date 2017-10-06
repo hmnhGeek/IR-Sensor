0d3607fc1de8b5858343f1088d28903f03037cbe
Himanshu Sharma <hmnhsharma97@gmail.com>
2017-10-06 23:18:28 +0530

    Add files via upload

---
+++ IR_sensor.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(2) == HIGH){
+      digitalWrite(13, HIGH);
+      digitalWrite(12, LOW);
+      digitalWrite(11, HIGH);
+    }
+  else{
+    digitalWrite(12, HIGH);
+      digitalWrite(13, LOW);
+      digitalWrite(11, LOW);
+    }
+  //delay(5000);
+}
